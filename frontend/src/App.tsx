35c58c8384874bdb21ce67d03a28fe5418aebf1b
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2022-09-05 21:54:49 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/notification-button"
+
 function App() {
   return (
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton></NotificationButton>
+    </>
   )
 }
 
